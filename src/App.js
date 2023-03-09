170f1193311ef4755f2c6bf5855ef181fa5c0cbd
Vivy <Tomitaviorel@yahoo.com>
2023-03-09 11:28:58 +0200

    feat:navigation is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Box } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
 import './app.css'
+import Footer from './component/footer'
+import Navbar from './component/navbar'
+import ExerciseDetail from './pages/exercisedetail'
+import Home from './pages/home'
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
---
+++ src/pages/exercisedetail.jsx
@@ -0,0 +1,5 @@
+const ExerciseDetail = () => {
+  return <div>Exercises</div>;
+};
+
+export default ExerciseDetail;
